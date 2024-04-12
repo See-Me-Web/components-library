83c2a34a63670ea4aa933b560ed371cdc0a29506
abareja <barejaadrian@gmail.com>
2024-04-12 18:33:57 +0200

    Feat: created partners slider block

---
+++ resources/views/blocks/partners-slider.blade.php
@@ -0,0 +1,12 @@
+<section 
+  id="{{ $block->block->anchor ?? $block->block->id }}"
+  style="{{ $style }}"
+>
+  <x-seeme::wrap>
+    <InnerBlocks allowedBlocks="{{ wp_json_encode($allowedBlocksBefore) }}" />
+
+    <InnerBlocks allowedBlocks="{{ wp_json_encode($allowedBlocksInner) }}" />
+
+    <InnerBlocks allowedBlocks="{{ wp_json_encode($allowedBlocksAfter) }}" />
+  </x-seeme::wrap>
+</section>
---
+++ src/Blocks/PartnersSlider.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Seeme\Components\Blocks;
+
+use Seeme\Components\Blocks\Abstract\BaseBlock;
+use Seeme\Components\Providers\CoreServiceProvider;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+class PartnersSlider extends BaseBlock
+{
+    /**
+     * The block name.
+     *
+     * @var string
+     */
+    public $name = 'PartnersSlider';
+
+    /**
+     * The block view.
+     */
+    public $view = CoreServiceProvider::NAMESPACE . '::blocks.partners-slider';
+
+    /**
+     * The block description.
+     *
+     * @var string
+     */
+    public $description = 'Partners slider';
+
+    /**
+     * The block icon.
+     *
+     * @var string|array
+     */
+    public $icon = 'info';
+
+    /**
+     * The block keywords.
+     *
+     * @var array
+     */
+    public $keywords = [
+        'partners',
+        'slider'
+    ];
+
+    /**
+     * The block post type allow list.
+     *
+     * @var array
+     */
+    public $post_types = [];
+
+    /**
+     * The parent block type allow list.
+     *
+     * @var array
+     */
+    public $parent = [];
+
+    /**
+     * The default block mode.
+     *
+     * @var string
+     */
+    public $mode = 'preview';
+
+    /**
+     * The default block alignment.
+     *
+     * @var string
+     */
+    public $align = '';
+
+    /**
+     * The default block text alignment.
+     *
+     * @var string
+     */
+    public $align_text = '';
+
+    /**
+     * The default block content alignment.
+     *
+     * @var string
+     */
+    public $align_content = '';
+
+    /**
+     * The supported block features.
+     *
+     * @var array
+     */
+    public $supports = [
+        'align' => true,
+        'align_text' => false,
+        'align_content' => false,
+        'full_height' => false,
+        'anchor' => true,
+        'mode' => true,
+        'multiple' => true,
+        'jsx' => true,
+        'spacing' => [
+          'padding' => true,
+          'margin' => true
+        ],
+        'color' => [
+          'text' => true,
+          'background' => true
+        ]
+    ];
+
+    /**
+     * The block styles.
+     *
+     * @var array
+     */
+    public $styles = [];
+
+    /**
+     * Data to be passed to the block before rendering.
+     *
+     * @return array
+     */
+    public function with()
+    {
+        return [
+          'allowedBlocksBefore' => [
+            'acf/heading'
+          ],
+          'allowedBlocksInner' => [
+            'core/image'
+          ],
+          'allowedBlocksAfter' => [
+            'acf/buttons',
+            'acf/button'
+          ],
+          'style' => $this->getStyles()
+        ];
+    }
+
+    /**
+     * The block field group.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        $slider = new FieldsBuilder('partners-slider');
+
+        return $slider->build();
+    }
+}
